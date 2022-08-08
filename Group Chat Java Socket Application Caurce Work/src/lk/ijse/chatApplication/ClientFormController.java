3429897d40b7bd806cdf4e6cd6db3cac7e960818
wishalNipun <wishal1974@gmail.com>
2022-08-08 14:39:50 +0530

    1st step updated

--- Group Chat Java Socket Application Caurce Work/src/lk/ijse/chatApplication/ClientFormController.java
+++ Group Chat Java Socket Application Caurce Work/src/lk/ijse/chatApplication/ClientFormController.java
@@ -4,10 +4,44 @@ import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientFormController {
     public JFXTextField txtMessage;
     public JFXTextArea textArea;
 
-    public void btnSendOnAction(ActionEvent actionEvent) {
+    final int PORT = 5000;
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+
+    String message = "";
+
+    public void initialize(){
+        textArea.setEditable(false);
+        new Thread(() -> {
+            try {
+                socket = new Socket("localhost",PORT);
+
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                while (true){
+                    message = dataInputStream.readUTF();
+                    textArea.appendText("\n Server : " + message);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+    public void btnSendOnAction(ActionEvent actionEvent) throws IOException {
+        dataOutputStream.writeUTF(txtMessage.getText().trim());
+        dataOutputStream.flush();
+        txtMessage.clear();
     }
 }
--- Group Chat Java Socket Application Caurce Work/src/lk/ijse/chatApplication/ServerApp.java
+++ Group Chat Java Socket Application Caurce Work/src/lk/ijse/chatApplication/ServerApp.java
@@ -1,7 +1,43 @@
 package lk.ijse.chatApplication;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApp {
     public static void main(String[] args) {
+        final int PORT = 5000;
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ServerSocket serverSocket = new ServerSocket(PORT);
+                    System.out.println("Server is running in port: " + PORT);
+
+                    Socket localSocket = serverSocket.accept();
+                    System.out.println("Client accepted..!");
+
+                    DataOutputStream dataOutputStream = new DataOutputStream(localSocket.getOutputStream());
+                    DataInputStream dataInputStream = new DataInputStream(localSocket.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+                    String message = "", reply = "";
+
+                    while (true) {
+                        message = dataInputStream.readUTF();
+                        System.out.println("Client says , :" + message);
+                        reply = bufferedReader.readLine();
+                        dataOutputStream.writeUTF(reply);
+                        dataOutputStream.flush();
+                    }
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
+
 }
