c2af3f7fc51b4601582cb221de000697aa1085a1
wishalNipun <wishal1974@gmail.com>
2022-08-08 14:10:46 +0530

    added server app class and clientfxml interfce view

--- Group Chat Java Socket Application Caurce Work/src/ClientFormController.java
+++ Group Chat Java Socket Application Caurce Work/src/ClientFormController.java
@@ -1,2 +1,11 @@
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class ClientFormController {
+    public JFXTextField txtMessage;
+    public JFXTextArea textArea;
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
 }
