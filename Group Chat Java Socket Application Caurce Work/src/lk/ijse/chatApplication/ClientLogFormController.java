88c66ae23c96371d53f13802b5a5eafee35900eb
wishalNipun <wishal1974@gmail.com>
2022-08-09 12:18:27 +0530

    content update

--- Group Chat Java Socket Application Caurce Work/src/lk/ijse/chatApplication/ClientLogFormController.java
+++ Group Chat Java Socket Application Caurce Work/src/lk/ijse/chatApplication/ClientLogFormController.java
@@ -1,4 +1,11 @@
 package lk.ijse.chatApplication;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class ClientLogFormController {
+    public JFXTextField txtUserName;
+
+    public void btnLogOnAction(ActionEvent actionEvent) {
+    }
 }
--- Group Chat Java Socket Application Caurce Work/src/lk/ijse/chatApplication/ServerApp.java
+++ Group Chat Java Socket Application Caurce Work/src/lk/ijse/chatApplication/ServerApp.java
@@ -1,7 +1,45 @@
 package lk.ijse.chatApplication;
 
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerApp {
+    final int PORT = 1234;
+    private ServerSocket serverSocket;
+    public ServerApp(ServerSocket serverSocket){
+        this.serverSocket =serverSocket;
+    }
+    public void startServer(){
+        try {
+            while (!serverSocket.isClosed()){
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected");
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        }catch (IOException e){
+
+        }
+    }
+
+    public void closeServerSocket(){
+        try {
+            if (serverSocket !=null){
+                serverSocket.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        System.out.println("Server is Running");
+        ServerApp serverApp = new ServerApp(serverSocket);
+        serverApp.startServer();
+    }
 
 }
